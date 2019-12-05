20b6ebf79aa9f343d1bd5825fdbebaae56355982
Vincent Couallier <vincent.couallier@math.u-bordeaux.fr>
2019-12-05 09:56:48 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,16 +1,16 @@
-package <- c("shiny","shinydashboard","ggplot2","dplyr","plyr","FactoMineR","GGally")
-new.packages <- package[!(package %in% installed.packages()[,"Package"])]
-if (length(new.packages)) install.packages(new.packages, repos='https://cloud.r-project.org')
-lapply(package, library, character.only=T)
-rm(list = c("package","new.packages"))
+#package <- c("shiny","shinydashboard","ggplot2","dplyr","plyr","FactoMineR","GGally")
+#new.packages <- package[!(package %in% installed.packages()[,"Package"])]
+#if (length(new.packages)) install.packages(new.packages, repos='https://cloud.r-project.org')
+#lapply(package, library, character.only=T)
+#rm(list = c("package","new.packages"))
 
-library(shiny)
-require(shinydashboard)
-library(ggplot2)
-library(dplyr)
-library(plyr)
-library(FactoMineR)
-library(GGally)
+#library(shiny)
+#require(shinydashboard)
+#library(ggplot2)
+#library(dplyr)
+#library(plyr)
+#library(FactoMineR)
+#library(GGally)
 
 function(input, output, session) {
 
