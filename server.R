44df2677f451847b10f63e33ed86f6e9bfe7aa75
Vincent Couallier <vincent.couallier@math.u-bordeaux.fr>
2019-12-17 13:36:47 +0100

    Update server.R

--- server.R
+++ server.R
@@ -24,7 +24,7 @@ function(input, output, session) {
   })
 
   output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+    palette(c("pink", "#377EB8", "#4DAF4A", "#984EA3",
       "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
     par(mar = c(5.1, 4.1, 0, 1))
